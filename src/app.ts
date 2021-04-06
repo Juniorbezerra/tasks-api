de81ce552dc077f6ceba266e18dc9ea89364b648
Junior Bezerra <juniorbezerra38@gmail.com>
2021-04-06 16:16:02 -0300

    config database

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { NextFunction, Request, Response } from 'express'
+import createConnection from './database'
+import { AppError } from './errors/AppError'
+
+const app = express()
+createConnection()
+
+app.use(express.json())
+
+app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'Error',
+    message: `Internal server error ${err.message}`
+  })
+}
+)
+
+export default app
